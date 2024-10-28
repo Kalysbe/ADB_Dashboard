ccd5f699b8e8a380aa9d7ba9e3a174aeee86609c
rrkalysbe@gmail.com <rrkalysbe@gmail.com>
2024-10-29 01:08:28 +0600

    change api

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instanse = axios.create({
-  baseURL: 'http://localhost:4444/'
+  baseURL: 'https://adb-solution.com/api/'
   // baseURL: process.env.REACT_APP_API_URL,
 
 
